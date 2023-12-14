37bccdda17ecf4222e84493165896ec3722d6fbd
Lucas Marques <lucasm.desenvolvedor@gmail.com>
2023-12-14 13:47:45 -0300

    Ipresb

---
+++ POS-RELEASE-BUSCADOR-01_Buscador- Ipresb/cypress.config.js
@@ -0,0 +1,16 @@
+
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    experimentalStudio: true,
+    projectId: "rjeym7",
+
+    // "retries": 5,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    defaultCommandTimeout: 90000,
+
+  },
+});
